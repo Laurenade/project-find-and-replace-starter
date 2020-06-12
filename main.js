1383438c85de7547247f47e2622b3790c97ccd96
Lauren <laurenade8@yahoo.com>
2020-06-11 22:39:10 -0500

    most javascript done

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const replaceAllButton = document.querySelector(".replace-all-button")
 // The following variable holds your OUTER ARRAY of row elements.
 // Later you will need an OUTER LOOP to loop over the individual elements within
 // this array.
+
 const rowElements = document.querySelectorAll(".row")
 
 // When you call the function belwo, it will get and return an INNER ARRAY
@@ -15,12 +16,66 @@ const rowElements = document.querySelectorAll(".row")
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
-    return currentRowElement.querySelectorAll(".cell")
-}
+/* 
+    function getCellElements (currentRowElement) {
+        return currentRowElement.querySelectorAll(".cell")
+    }
+*/
 
 
 // YOUR CODE GOES HERE
+
+replaceAllButton.addEventListener("click", function() {
+    let findBoxValue = findInput.value
+    let replaceBoxValue = replaceInput.value
+    const cellsArray = []
+    for (let position = 1; position <= rowElements.length; position += 1) {
+            currentRowElement = rowElements[position]
+            function getCellElements (currentRowElement) {
+                return currentRowElement.querySelectorAll(".cell")               
+            }
+            cellsArray.push(getCellElements)
+            for (let position= 1; position <= cellsArray.length; position += 1) {
+                if (cellsArray.includes(findBoxValue)) {
+                    return cellsArray.innerHTML.replace(findBoxValue, replaceBoxValue)
+                }
+            }
+    }
+    //   console.log('good')
+})
+
+/*  
+loop over rowElements 
+getCellElements function used within rowElements loop
+    - will return an array of cell info for a row
+loop over cell info
+
+GOAL:  Add a basic "find & replace" feature to our app
+- User enters word/value into 'find' box.
+- User enters word/value into 'replace' box.
+- User clicks 'replace all' button.
+    - Retrieve values entered into boxes.
+    - A loop goes through the document and locates all instances of value entered 
+        and pushes to array.
+    - A loop goes through created array and changes all original values to new/replace value.
+- All instances of word/value in 'find' box is replaced with the word/value in 'replace' box.
+
+// -----1. Add a click event listener to the replaceAllButton
+// -----2. On click, but outside of a loop, assign the values of the two input 
+//        textboxes (findInput and replaceInput), so that you are only retrieving 
+//        these values once per click.
+// -----3. Write a loop which loops over the rowElements array (which is already 
+//        provided in the starter code).
+// -----4. Inside this loop, use the getCellElements() function (already provided in the 
+//        starter code) and assign the resulting array of cell elements to a variable.
+// -----5. Write a nested loop which loops over the array of cell elements.
+// -----6. For each cell element, check if a cell contains the user-provided search 
+//        string. Use the string method includes().
+// -----7. If a cell does contain the user-provided search string, use innerHTML and the 
+//        string method replace() to replace the user-provided search string with 
+//        the user-provided replacement string.
+// -----8. In all your loops, avoid using low-meaning index variables like "i" and "j".
+*/
 
 
 // One last thing: dedicate very careful attention to using variables and
